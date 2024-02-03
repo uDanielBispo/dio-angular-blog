fc79ff912305f897750c3f66c3948b0b7b86e3a8
Daniel Bispo <danielbispo3015@hotmail.com>
2024-02-03 09:50:42 -0300

    creating components and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BigCardComponent } from './component/big-card/big-card.component';
+import { MenuTitleComponent } from './component/menu-title/menu-title.component';
+import { SmallCardComponent } from './component/small-card/small-card.component';
+import { MenuBarComponent } from './component/menu-bar/menu-bar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BigCardComponent,
+    MenuTitleComponent,
+    SmallCardComponent,
+    MenuBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
